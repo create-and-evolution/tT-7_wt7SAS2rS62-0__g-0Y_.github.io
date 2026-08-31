d86ca47d8f427bea6877dbd6d8dfb3c83db37531
Vava-create <chafaantie.said@gmail.com>
2026-08-31 17:46:36 +0200

    Deploiement par GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
+  base: '/tT-7_wt7SAS2rS62-0__g-0Y_.github.io/',
 })
